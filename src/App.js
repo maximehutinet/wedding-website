ed7f0286b880e183edfdd388bbc3e0123674ce7b
Maxime Hutinet <maxime@hutinet.ch>
2021-07-31 16:59:18 +0200

    Removed unused references.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.scss';
-import {useTranslation} from "react-i18next";
 import React from "react";
 import Nav from './components/Nav.js';
 import HeaderImage from './components/HeaderImage.js';
 import Intro from "./components/Intro";
-import Map from "./components/Map";
 import IntroTMB from "./components/IntroTMB";
 import FullWidthImage from "./components/FullWidthImage";
 import tmbImage from './img/header_image.jpg'
-
-/*function Header() {
-  const {t, i18n} = useTranslation('common');
-  return <div>
-    <h1>{t('welcome.title', {framework:'React'})}</h1>
-    <button onClick={() => i18n.changeLanguage('fr')}>fr</button>
-    <button onClick={() => i18n.changeLanguage('en')}>en</button>
-  </div>
-}*/
 
 function App() {
   return (
