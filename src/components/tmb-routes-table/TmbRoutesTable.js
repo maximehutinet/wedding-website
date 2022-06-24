9b4431945dc4910eb2a23814d3e794b69e53bf96
Maxime Hutinet <maxime@hutinet.fr>
2022-06-24 15:45:37 +0200

    Updated TMB infos

--- src/components/tmb-routes-table/TmbRoutesTable.js
+++ src/components/tmb-routes-table/TmbRoutesTable.js
@@ -20,17 +20,17 @@ const useStyles = makeStyles({
 });
 
 const rows = [
-    createData('08.26.2022', '26.08.2022', 1, 'Les Houches', true,'Les Contamines', true,17, 1010),
-    createData('08.27.2022', '27.08.2022', 2, 'Les Contamines', true,'Refuge de la Croix du Bonhomme', false,20, 1360),
-    createData('08.28.2022', '28.08.2022', 3, 'Refuge de la Croix du Bonhomme', false,'Refugio Elisabetta', false,14, 1100),
-    createData('08.29.2022', '29.08.2022',4, 'Refugio Elisabetta', false,'Rifugio Maison Vieille', false,16, 580),
-    createData('08.30.2022', '30.08.2022', 5, 'Rifugio Maison Vieille', false,'Refugio Bonatti', false,12, 1080),
-    createData('08.31.2022', '31.08.2022', 6, 'Refugio Bonatti', false,'La Fouly (Hôtel Edelweiss)', true,20, 980),
-    createData('09.01.2022', '01.09.2022', 7, 'La Fouly (Hôtel Edelweiss)', true,'Relais d\'Arpette', false,16, 510),
-    createData('09.02.2022', '02.09.2022', 8, 'Relais d\'Arpette', false,'Trient (Hôtel La Grande Ourse)', true,16, 1200),
-    createData('09.03.2022', '03.09.2022', 9, 'Trient (Hôtel La Grande Ourse)', true,'Tré-le-Champ (Auberge La Boërne)', true,14, 980),
-    createData('09.04.2022', '04.09.2022', 10, 'Tré-le-Champ (Auberge La Boërne)', true,'Refuge du Lac Blanc', false,8, 730),
-    createData('09.05.2022', '05.09.2022', 11, 'Refuge du Lac Blanc', false, 'Les Houches', true,8, 730),
+    createData('08.26.2022', '26.08.2022', 1, 'Les Houches', true,'Refuge de Miage', true,12, 1366),
+    createData('08.27.2022', '27.08.2022', 2, 'Refuge de Miage', true,'Refuge de la Croix du Bonhomme', false,18, 1500),
+    createData('08.28.2022', '28.08.2022', 3, 'Refuge de la Croix du Bonhomme', false,'Refugio Elisabetta', false,15, 966),
+    createData('08.29.2022', '29.08.2022',4, 'Refugio Elisabetta', false,'Rifugio Maison Vieille', false,10, 580),
+    createData('08.30.2022', '30.08.2022', 5, 'Refugio Maison Vieille', false,'Refugio Bonatti', false,20, 1600),
+    createData('08.31.2022', '31.08.2022', 6, 'Refugio Bonatti', false,'La Fouly (Hôtel Edelweiss)', true,18, 900),
+    createData('09.01.2022', '01.09.2022', 7, 'La Fouly (Hôtel Edelweiss)', true,'Relais d\'Arpette', false,17, 897),
+    createData('09.02.2022', '02.09.2022', 8, 'Relais d\'Arpette', false,'Trient (Hôtel La Grande Ourse)', true,13, 1358),
+    createData('09.03.2022', '03.09.2022', 9, 'Trient (Hôtel La Grande Ourse)', true,'Tré-le-Champ (Auberge La Boërne)', true,11, 991),
+    createData('09.04.2022', '04.09.2022', 10, 'Tré-le-Champ (Auberge La Boërne)', true,'Refuge du Lac Blanc', false,5.7, 977),
+    createData('09.05.2022', '05.09.2022', 11, 'Refuge du Lac Blanc', false, 'Les Houches', true,19, 921),
 ];
 
 function createData(dateEn, dateFr, day, startingPoint, startAccessibleByCar, endingPoint, endAccessibleByCar, distance, elevation) {
