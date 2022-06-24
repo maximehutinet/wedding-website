2b06634134c99c2ec867a7a11981c313ffcb107e
Maxime Hutinet <maxime@hutinet.fr>
2022-06-24 20:28:14 +0200

    Modified day designation.

--- src/components/pages/home/components/intro/intro.js
+++ src/components/pages/home/components/intro/intro.js
@@ -1,9 +1,12 @@
 import {useTranslation} from "react-i18next";
 import './intro.scss'
 import React from "react";
+import i18next from "i18next";
 
 function Intro() {
     const {t} = useTranslation('common');
+    const currentLanguage = i18next.language;
+    const dayDesignation = currentLanguage === 'en' ? "D" : "J";
     const weddingDate = new Date("09/08/2022");
     const now = new Date();
     const oneDay = 1000 * 3600 * 24;
@@ -14,7 +17,7 @@ function Intro() {
             {timeBeforeWedding > 0 &&
                 <div id="wedding-coming-up">
                     <span>{t('intro.wedding_coming_up')}</span>
-                    <span id="time-left-before-wedding">J-{timeBeforeWedding}</span>
+                    <span id="time-left-before-wedding">{dayDesignation}-{timeBeforeWedding}</span>
                     <a id="rsvp-btn" href="https://docs.google.com/forms/d/e/1FAIpQLScvwwnOKSGUkM85Je8UuhOFFLzxADq2GLB7lnIWLl4ry-IG5Q/viewform">{t('intro.rsvp_btn')}</a>
                 </div>
             }
