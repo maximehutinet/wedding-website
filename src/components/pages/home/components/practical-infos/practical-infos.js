ae8d0d9d2ac9c6b815473655169a0d9a265043c5
Maxime Hutinet <maxime@hutinet.fr>
2022-07-11 18:56:15 +0200

    Added dress code section.

--- src/components/pages/home/components/practical-infos/practical-infos.js
+++ src/components/pages/home/components/practical-infos/practical-infos.js
@@ -10,6 +10,7 @@ import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import FullWidthImage from "../../../../common/full-width-image/FullWidthImage";
 import giftImage from "../../../../../img/gift.jpg";
+import dressCodeImage from "../../../../../img/dess_code.jpeg";
 
 function PracticalInfos() {
     const {t} = useTranslation('common');
@@ -28,7 +29,8 @@ function PracticalInfos() {
             <div id="where-do-we-meet-wrapper">
                 <h2>{t('practical_infos.where_do_we_meet')}</h2>
                 <div id="meeting-point">
-                    {t('practical_infos.in_front_of')} <a href="https://goo.gl/maps/di9HkMD8HftHQcMA8" target="_blank" rel="noreferrer">Banque Populaire
+                    {t('practical_infos.in_front_of')} <a href="https://goo.gl/maps/di9HkMD8HftHQcMA8" target="_blank"
+                                                          rel="noreferrer">Banque Populaire
                     Auvergne Rhône Alpes - 331 Rte des Grandes Alpes, 74220 La Clusaz</a>
                     <strong>{t('practical_infos.meeting_date_time')}</strong>
                 </div>
@@ -43,7 +45,9 @@ function PracticalInfos() {
             <div id="where-can-i-park-wrapper">
                 <h2>{t('practical_infos.parking_title')}</h2>
                 <p>
-                    {t('practical_infos.parking_content_1')} <a href="https://goo.gl/maps/jKkJfY3QEHWQpKXG9" target="_blank" rel="noreferrer">Parking centre village</a>. {t('practical_infos.parking_content_2')}
+                    {t('practical_infos.parking_content_1')} <a href="https://goo.gl/maps/jKkJfY3QEHWQpKXG9"
+                                                                target="_blank" rel="noreferrer">Parking centre
+                    village</a>. {t('practical_infos.parking_content_2')}
                 </p>
                 <div className="iframe-wrapper">
                     <iframe
@@ -62,7 +66,7 @@ function PracticalInfos() {
                         </TimelineOppositeContent>
                         <TimelineSeparator>
                             <TimelineDot/>
-                            <TimelineConnector />
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_pickup')}</TimelineContent>
                     </TimelineItem>
@@ -71,8 +75,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_ceremony')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="primary" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="primary"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_ceremony')}</TimelineContent>
                     </TimelineItem>
@@ -81,8 +85,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_shuttle_venue')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="error" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="error"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_shuttle_venue')}</TimelineContent>
                     </TimelineItem>
@@ -91,8 +95,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_vin')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="secondary" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="secondary"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_vin')}</TimelineContent>
                     </TimelineItem>
@@ -101,8 +105,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_dinner')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="success" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="success"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_dinner')}</TimelineContent>
                     </TimelineItem>
@@ -111,8 +115,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_party')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="warning" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="warning"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_party')}</TimelineContent>
                     </TimelineItem>
@@ -121,8 +125,8 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_snacks')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot variant="outlined" color="primary" />
-                            <TimelineConnector />
+                            <TimelineDot variant="outlined" color="primary"/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_snacks')}</TimelineContent>
                     </TimelineItem>
@@ -131,24 +135,63 @@ function PracticalInfos() {
                             {t('practical_infos.plan_time_party_over')}
                         </TimelineOppositeContent>
                         <TimelineSeparator>
-                            <TimelineDot />
+                            <TimelineDot/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_party_over')}</TimelineContent>
                     </TimelineItem>
                 </Timeline>
             </div>
             <hr/>
+            <div id="dress-code-wrapper">
+                <h2>{t('practical_infos.dress_code_title')}</h2>
+                <img src={dressCodeImage} alt="dress code"/>
+                <div>
+                    <p>
+                        {t('practical_infos.dress_code_summary')}
+                    </p>
+
+                    <label>{t('practical_infos.dress_code_women')}</label>
+                    <ul>
+                        <li>{t('practical_infos.dress_code_women_shoes')}</li>
+                        <li>{t('practical_infos.dress_code_women_dress')}</li>
+                        <li>{t('practical_infos.dress_code_women_rompers')}</li>
+                        <li>{t('practical_infos.dress_code_women_pant_suits')}</li>
+                    </ul>
+
+                    <label>{t('practical_infos.dress_code_men')}</label>
+                    <ul>
+                        <li>{t('practical_infos.dress_code_men_shirt')}</li>
+                        <li>{t('practical_infos.dress_code_men_slacks')}</li>
+                        <li>{t('practical_infos.dress_code_men_suit')}</li>
+                    </ul>
+
+                    <label>{t('practical_infos.dress_code_not_wear')}</label>
+                    <ul>
+                        <li>Jeans</li>
+                        <li>Shorts</li>
+                        <li>{t('practical_infos.dress_code_flip_flops')}</li>
+                        <li>{t('practical_infos.dress_code_polo')}</li>
+                        <li>T-shirts</li>
+                        <li>{t('practical_infos.dress_code_hoodies')}</li>
+                    </ul>
+                </div>
+            </div>
+            <hr/>
             <div id="registry-wrapper">
                 <h2>{t('practical_infos.registry_title')}</h2>
                 <div>
                     <p>{t('practical_infos.registry_content')}</p>
-                    <a className="btn" href="http://www.leetchi.com/c/sarah-maxs-wedding" target="_blank" rel="noreferrer">{t('practical_infos.registry_btn')}</a>
+                    <a className="btn" href="http://www.leetchi.com/c/sarah-maxs-wedding" target="_blank"
+                       rel="noreferrer">{t('practical_infos.registry_btn')}</a>
                 </div>
             </div>
             <FullWidthImage image={giftImage}/>
             <div id="welcome-drink-wrapper">
                 <h2>{t('practical_infos.welcome_drink_title')}</h2>
-                <p>{t('practical_infos.welcome_drink_content')} <strong>{t('practical_infos.welcome_drink_time')}</strong> {t('practical_infos.welcome_drink_our_place')} <br/><br/><a href="https://goo.gl/maps/z8x8CFZ22iySfYEi9" target="_blank" rel="noreferrer">639 Route du Col de la Croix Fry, 74220 La Clusaz</a> </p>
+                <p>{t('practical_infos.welcome_drink_content')}
+                    <strong>{t('practical_infos.welcome_drink_time')}</strong> {t('practical_infos.welcome_drink_our_place')}
+                    <br/><br/><a href="https://goo.gl/maps/z8x8CFZ22iySfYEi9" target="_blank" rel="noreferrer">639 Route
+                        du Col de la Croix Fry, 74220 La Clusaz</a></p>
             </div>
             <hr/>
             <div id="plan-coming-days">
@@ -160,7 +203,7 @@ function PracticalInfos() {
                         </TimelineOppositeContent>
                         <TimelineSeparator>
                             <TimelineDot variant="outlined" color="primary"/>
-                            <TimelineConnector />
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>Welcome drink</TimelineContent>
                     </TimelineItem>
@@ -170,7 +213,7 @@ function PracticalInfos() {
                         </TimelineOppositeContent>
                         <TimelineSeparator>
                             <TimelineDot variant="outlined" color="secondary"/>
-                            <TimelineConnector />
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_coming_days_wedding_day')}</TimelineContent>
                     </TimelineItem>
@@ -180,7 +223,7 @@ function PracticalInfos() {
                         </TimelineOppositeContent>
                         <TimelineSeparator>
                             <TimelineDot/>
-                            <TimelineConnector />
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_coming_days_rest_day')}</TimelineContent>
                     </TimelineItem>
@@ -190,7 +233,7 @@ function PracticalInfos() {
                         </TimelineOppositeContent>
                         <TimelineSeparator>
                             <TimelineDot variant="outlined" color="success"/>
-                            <TimelineConnector />
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>{t('practical_infos.plan_coming_days_mtb_day')}</TimelineContent>
                     </TimelineItem>
@@ -208,7 +251,9 @@ function PracticalInfos() {
             <hr/>
             <div id="la-clusaz-infos-wrapper">
                 <h2>{t('practical_infos.la_clusaz_infos_title')}</h2>
-                <p>{t('practical_infos.la_clusaz_infos_content')} <a href="https://en.laclusaz.com/events-and-activities.html" target="_blank" rel="noreferrer">{t('practical_infos.la_clusaz_infos_link')}</a></p>
+                <p>{t('practical_infos.la_clusaz_infos_content')} <a
+                    href="https://en.laclusaz.com/events-and-activities.html" target="_blank"
+                    rel="noreferrer">{t('practical_infos.la_clusaz_infos_link')}</a></p>
             </div>
         </div>
     )
