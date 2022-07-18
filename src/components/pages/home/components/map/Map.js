f414b98dcb5b456c14501fb733e92083cb3ca3eb
Maxime Hutinet <maxime@hutinet.fr>
2022-07-18 21:44:43 +0200

    Updated maps. Added food & water section.

--- src/components/pages/home/components/map/Map.js
+++ src/components/pages/home/components/map/Map.js
@@ -11,7 +11,10 @@ function Map() {
         <div className="map-section">
             <h2>{t('map_table_tmb.title')}</h2>
             <div className="map-wrapper">
-                <iframe title="map" width="1120" height="800" frameBorder="0" src="https://fatmap.com/guidebooks/50380/tour-du-mont-blanc-sarah-and-max-wedding-2022"/>
+                <iframe title="map" width="1120" height="800" src="https://fatmap.com/routeid/2999543/tour-du-mont-blanc-2022?fmid=em"/>
+            </div>
+            <div className="map-wrapper">
+                <iframe title="map google" src="https://www.google.com/maps/d/u/0/embed?mid=1H90vQ-EETqUB_FBRIKra9oq2Y9VO-HM&ehbc=2E312F" width="1120" height="800"></iframe>
             </div>
             <div className="buttons-wrapper">
                 {currentLanguage === 'en' && <Button variant="outlined" color="primary" target="_blank" href="https://www.amazon.com/Trekking-Tour-Mont-Blanc-guidebook-ebook/dp/B08B8CPMC7">Trekking guide</Button> }
--- src/components/pages/tmb/tmb-page.js
+++ src/components/pages/tmb/tmb-page.js
@@ -24,33 +24,53 @@ function TmbPage() {
                 <p>{t('tmb-page.recommended-hiking-bag')}</p>
                 <h3>{t('tmb-page.gear-recommended-title-all')}</h3>
                 <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.rain-pants')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.puff-jacket')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.hiking-pants')} />
-                    <FormControlLabel control={<Checkbox />} label="T-Shirt x2" />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.mid-layer')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.gloves')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.hat')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.sunglasses')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.sunscreen')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.hiking-poles')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.headlamps')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.safety-blanket')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.shoes')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.liner')} />
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.rain-pants')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.puff-jacket')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.hiking-pants')}/>
+                    <FormControlLabel control={<Checkbox/>} label="T-Shirt x2"/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.mid-layer')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.gloves')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.hat')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.sunglasses')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.sunscreen')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.hiking-poles')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.headlamps')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.safety-blanket')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.shoes')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.liner')}/>
                 </FormGroup>
 
                 <h3>{t('tmb-page.gear-recommended-title-camper')}</h3>
                 <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.tent')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.sleeping-page')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.sleeping-bag')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.trash-bag')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.spoon')} />
-                    <FormControlLabel control={<Checkbox />} label="Mug" />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.knife')} />
-                    <FormControlLabel control={<Checkbox />} label={t('tmb-page.stove')} />
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.tent')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.sleeping-page')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.sleeping-bag')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.trash-bag')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.spoon')}/>
+                    <FormControlLabel control={<Checkbox/>} label="Mug"/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.knife')}/>
+                    <FormControlLabel control={<Checkbox/>} label={t('tmb-page.stove')}/>
                 </FormGroup>
+
+                <hr/>
+                <h2>Food & water</h2>
+                <ul>
+                    <li>
+                        Campers, considers bringing 1 day of food with you. We will pass by villages as the two maps
+                        show
+                        above but it can happen that we won't find food. Make sure also to bring breakfast !
+                    </li>
+                    <li>
+                        For the refuger, you'll have breakfast and dinner at the huts. However, bring some snacks and
+                        potentially food for at least one lunch in case we don't find food for launch on the way.
+                    </li>
+                    <li>
+                        There are a bunch of stream of water along the way. We usually drink straight from them but if
+                        you wanna be more cautious, we recommend to filter it as there could be some herd upstream.
+                    </li>
+                </ul>
+                <p></p>
+                <p></p>
             </section>
         </div>
     );
